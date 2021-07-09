1265258fd118a15c9259978f3a3cdfe34d4bcc6a
Michael Uno <michael2@michaeluno.jp>
2021-07-09 13:32:59 +0900

    Release: Change the version to 1.3.8 for release

--- custom-scrollbar.php
+++ custom-scrollbar.php
@@ -5,7 +5,7 @@
  *  Description:    Adds a custom scrollbar to specified HTML elements.
  *  Author:         Michael Uno (miunosoft)
  *  Author URI:     http://michaeluno.jp
- *  Version:        1.3.7
+ *  Version:        1.3.8
  */
 
 /**
@@ -15,7 +15,7 @@
  */
 class CustomScrollbar_Registry_Base {
  
-	const VERSION        = '1.3.7';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+	const VERSION        = '1.3.8';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
 	const NAME           = 'Custom Scrollbar';
 	const DESCRIPTION    = 'Adds a custom scrollbar to specified HTML elements.';
 	const URI            = 'http://en.michaeluno.jp/custom-scrollbar';
